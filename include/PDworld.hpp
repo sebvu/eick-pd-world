7ddbdca0df5c9c842cee1f2ca0d4a03fb4db62d7
Jester Santos <santosjester738@gmail.com>
2026-02-23 23:34:32 -0600

    Update w/terminal state entries

--- include/PDworld.hpp
+++ include/PDworld.hpp
@@ -37,11 +37,21 @@ private:
   class StateWorld {
   private:
     int i, j, a, b, c, d, e, f;
-    bool x;
+    int ti, tj, ta, tb, tc, td, te, tf;
+    bool x, tx;
 
   public:
-    StateWorld(int i, int j, bool x, int a, int b, int c, int d, int e, int f)
+    StateWorld(int i, int j, bool x, int a, int b, int c, int d, int e, int f,
+               int ti, int tj, int tx, int ta, int tb, int tc, int td, int te,
+               int tf)
         : i(i), j(j), a(a), b(b), c(c), d(d), e(e), f(f), x(x) {}
+
+    bool TerminalStateReached() {
+      return (i == ti && j == tj && a == ta && b == tb && c == tc && d == td &&
+              e == te && f == tf && x == tx)
+                 ? true
+                 : false;
+    };
   };
 
   StateWorld state;
@@ -51,7 +61,8 @@ private:
 
 public:
   PDworld(int i, int j, bool x, int a, int b, int c, int d, int e, int f,
-          int gridX, int gridY);
+          int gridX, int gridY, int ti, int tj, bool tx, int ta, int tb, int tc,
+          int td, int te, int tf);
 };
 
 #endif
--- src/PDworld.cpp
+++ src/PDworld.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 
 PDworld::PDworld(int i, int j, bool x, int a, int b, int c, int d, int e, int f,
-                 int gridX, int gridY)
-    : state(StateWorld(i, j, x, a, b, c, d, e, f)), gridX(gridX), gridY(gridY) {
+                 int gridX, int gridY, int ti, int tj, bool tx, int ta, int tb,
+                 int tc, int td, int te, int tf)
+    : state(StateWorld(i, j, x, a, b, c, d, e, f, ti, tj, tx, ta, tb, tc, td,
+                       te, tf)),
+      gridX(gridX), gridY(gridY) {
   if (i > gridX || j > gridY) {
     throw std::invalid_argument("gridX or gridY does not abide with i, or j");
   }
