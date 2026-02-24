848f79060cfb132fc04c7542002d3f7517dba20d
Jester Santos <santosjester738@gmail.com>
2026-02-24 13:39:25 -0600

    Fix unitialized constructor warnings

--- src/PDworld.cpp
+++ src/PDworld.cpp
@@ -1,9 +1,9 @@
 #include "PDworld.hpp"
 #include <iostream>
 
-PDworld::PDworld(const PDstate initial, const PDstate terminal,
+PDworld::PDworld(const PDstate initialState, const PDstate terminalState,
                  const Rewards rewards, const double ALPHA, const double GAMMA,
                  const int GRID_I, const int GRID_J)
-    : initialState(initialState), terminalState(terminalState),
-      rewards(rewards), ALPHA(ALPHA), GAMMA(GAMMA), GRID_I(GRID_I),
-      GRID_J(GRID_J) {}
+    : ALPHA(ALPHA), GAMMA(GAMMA),
+      GRID_I(GRID_I), GRID_J(GRID_J), initialState(initialState), terminalState(terminalState),
+      rewards(rewards) {}
