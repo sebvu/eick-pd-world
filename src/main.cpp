2fe5bc3d2b0a6c73973acea7c532e5715b308fd7
Jester Santos <santosjester738@gmail.com>
2026-02-23 23:53:09 -0600

    Fix explanatory comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
 
   // initial state:(1,5,false,5,5,5,0,0,0)
   // terminal state: (*,*,false,0,0,0,5,5,5)
-  // (1, 5, false, 5, 5, 5, 0, 0, 0, (skipping *), false, .... etc)
+  // pickupReward, dropoffReward, nReward, sReward, eReward, wReward
 
   PDworld world = PDworld(1, 5, false, 5, 5, 5, 0, 0, 0, 5, 5, false, 0, 0, 0,
                           5, 5, 5, 13, 13, -1, -1, -1, -1);
