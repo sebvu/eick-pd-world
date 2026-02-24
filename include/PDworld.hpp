c4ce6ab93ade811eadeb6f20051fed91af77abf8
Jester Santos <santosjester738@gmail.com>
2026-02-24 13:39:59 -0600

    Comment patch for PDworld

--- include/PDworld.hpp
+++ include/PDworld.hpp
@@ -5,18 +5,18 @@
 
 class PDworld {
 private:
-  const double ALPHA; // learning rate
-  const double GAMMA; // discount rate
-  const int GRID_I;   // I size of PDworld
-  const int GRID_J;   // J size of PDworld
-  const PDstate initialState; // the initial state for all experiments
+  const double ALPHA;          // learning rate
+  const double GAMMA;          // discount rate
+  const int GRID_I;            // I size of PDworld
+  const int GRID_J;            // J size of PDworld
+  const PDstate initialState;  // the initial state for all experiments
   const PDstate terminalState; // the terminal state for all experiments
-  const Rewards rewards;
+  const Rewards rewards;       // rewards for different actions defined
 
 public:
-  PDworld(const PDstate initialState, const PDstate terminalState, const Rewards rewards,
-          const double ALPHA, const double GAMMA, const int GRID_I,
-          const int GRID_J);
+  PDworld(const PDstate initialState, const PDstate terminalState,
+          const Rewards rewards, const double ALPHA, const double GAMMA,
+          const int GRID_I, const int GRID_J);
 };
 
 #endif
