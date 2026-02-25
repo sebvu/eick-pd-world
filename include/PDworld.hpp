81ac2d45e012dc21c72fe14ed5b27158778dff8b
Jester Santos <santosjester738@gmail.com>
2026-02-24 18:55:04 -0600

    Move definition to PDworld.cpp

--- include/PDworld.hpp
+++ include/PDworld.hpp
@@ -39,40 +39,7 @@ private:
   };
 
   // return a set of applicable operations
-  std::vector<Action> aplop(const PDstate &s) {
-    std::vector<Action> ops;
-
-    // verify a cardinal direction
-    if (s.i > 1)
-      ops.push_back(Action::West);
-    if (s.i < GRID_I)
-      ops.push_back(Action::East);
-    if (s.j > 1)
-      ops.push_back(Action::North);
-    if (s.j < GRID_J)
-      ops.push_back(Action::South);
-
-    // lambda function to verify if s.i and s.j is in a *_loc pos
-    auto isAt = [&](std::pair<int, int> loc) {
-      return s.i == loc.first && s.j == loc.second;
-    };
-
-    // verify if pickup is valid
-    bool onPickupA = isAt({s.a_loc.first, s.a_loc.second});
-    bool onPickupB = isAt({s.b_loc.first, s.b_loc.second});
-    bool onPickupC = isAt({s.c_loc.first, s.c_loc.second});
-    if (s.x == false && (onPickupA || onPickupB || onPickupC))
-      ops.push_back(Action::Pickup);
-
-    // verify if dropoff is valid
-    bool onDropoffD = isAt({s.d_loc.first, s.d_loc.second});
-    bool onDropoffE = isAt({s.e_loc.first, s.d_loc.second});
-    bool onDropoffF = isAt({s.f_loc.first, s.f_loc.second});
-    if (s.x == true && (onDropoffD || onDropoffE || onDropoffF))
-      ops.push_back(Action::Dropoff);
-
-    return ops;
-  }
+  std::vector<Action> aplop(const PDstate &s);
 
 public:
   PDworld(const PDstate *initialState, const PDstate *terminalState,
--- src/PDworld.cpp
+++ src/PDworld.cpp
@@ -6,6 +6,41 @@ PDworld::PDworld(const PDstate *initialState, const PDstate *terminalState,
     : ALPHA(ALPHA), GAMMA(GAMMA), initialState(*initialState),
       terminalState(*terminalState), rewards(*rewards) {}
 
+std::vector<PDworld::Action> PDworld::aplop(const PDstate &s) {
+  std::vector<Action> ops;
+
+  // verify a cardinal direction
+  if (s.i > 1)
+    ops.push_back(Action::West);
+  if (s.i < GRID_I)
+    ops.push_back(Action::East);
+  if (s.j > 1)
+    ops.push_back(Action::North);
+  if (s.j < GRID_J)
+    ops.push_back(Action::South);
+
+  // lambda function to verify if s.i and s.j is in a *_loc pos
+  auto isAt = [&](std::pair<int, int> loc) {
+    return s.i == loc.first && s.j == loc.second;
+  };
+
+  // verify if pickup is valid
+  bool onPickupA = isAt({s.a_loc.first, s.a_loc.second});
+  bool onPickupB = isAt({s.b_loc.first, s.b_loc.second});
+  bool onPickupC = isAt({s.c_loc.first, s.c_loc.second});
+  if (s.x == false && (onPickupA || onPickupB || onPickupC))
+    ops.push_back(Action::Pickup);
+
+  // verify if dropoff is valid
+  bool onDropoffD = isAt({s.d_loc.first, s.d_loc.second});
+  bool onDropoffE = isAt({s.e_loc.first, s.d_loc.second});
+  bool onDropoffF = isAt({s.f_loc.first, s.f_loc.second});
+  if (s.x == true && (onDropoffD || onDropoffE || onDropoffF))
+    ops.push_back(Action::Dropoff);
+
+  return ops;
+}
+
 void PDworld::QLearning(std::vector<std::pair<int, std::string>> instructions) {
 }
 
